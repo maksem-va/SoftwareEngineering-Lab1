54012a4a78bd019ae7d4afc9646d542b24b1c7e3
maksem-va <senLu7zi>
2022-01-17 16:30:24 +0300

    Simple converter RUB-USD. Like in 2007

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-#Hello
+def cb_page_getter():
+    inp = input('Input request. ').split()
+    print(inp[0] + " " + inp[1].upper() + " equals " + str(float(inp[0]) * 30) + " RUB")
+
+
+# 30 рублей как в 2007
+
+if __name__ == '__main__':
+    cb_page_getter()
